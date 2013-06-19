eb0e77e92a93f805a76244be7fcdf1f6d8a19573
oolab <oolab@oolab-53.(none)>
2013-06-19 22:06:42 +0800

    complete sign-in and sign-out functionality

--- ncu_sign.js
+++ ncu_sign.js
@@ -2,16 +2,51 @@ var Browser = require("zombie");
 var assert = require("assert");
 var argv = require('optimist').argv;
 
-var signinURL = "http://140.115.182.62/PartTime/parttime.php/signin";
-var id = argv._[0];
-var pw = argv._[1];
+var signURL = "http://140.115.182.62/PartTime/parttime.php/";
+var action = argv._[0];
+var id = argv._[1];
+var pw = argv._[2];
+var projectValue = argv._[3];
+var browser = new Browser();
 
-browser = new Browser();
-browser.visit(signinURL, function() {
-  browser.
-    fill("j_username", id).
-    fill("j_password", pw).
-    pressButton("Login", function() {
-      console.log(browser.html());
-    });
-});
+function doSignIn(error) {
+  console.log(browser.html());
+  var projects = browser.queryAll("#checkin");
+  projects.forEach(function(proj) {
+    if (projectValue == proj.value && action == "signin") {
+      browser.choose("signin").pressButton("submit", function() {
+	console.log("Project value :" + proj.value + " signed in successful.");	
+	browser.close();
+      });
+    }
+  });
+
+}
+
+function doSignOut(error) {
+  console.log(browser.html());
+  browser.choose("signout").pressButton("submit", function() {
+    browser.close();
+  });
+}
+
+function doLogin() {
+  browser.visit(signURL + action, function() {
+    browser.
+      fill("j_username", id).
+      fill("j_password", pw).
+      pressButton("Login", function() {
+	if (action == "signin") {
+	  doSignIn();
+	} 
+	else if (action == "signout") {
+	  doSignOut();
+	} 
+	else {
+	  console.log("Invalid command");
+	}
+      });
+  });
+}
+
+doLogin();
