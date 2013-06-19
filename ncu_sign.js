d704c568620d9e4a884afbfc5b46a840eb6bf83b
Ricky Chien <ricky060709@gmail.com>
2013-06-20 01:22:27 +0800

    fix sign-in bug and reinstall zombie package.

--- ncu_sign.js
+++ ncu_sign.js
@@ -1,4 +1,4 @@
-var Browser = require("zombie");
+﻿var Browser = require("zombie");
 var assert = require("assert");
 var argv = require('optimist').argv;
 
@@ -6,25 +6,20 @@ var signURL = "http://140.115.182.62/PartTime/parttime.php/";
 var action = argv._[0];
 var id = argv._[1];
 var pw = argv._[2];
-var projectValue = argv._[3];
+var pid = argv._[3];
 var browser = new Browser();
 
 function doSignIn(error) {
-  console.log(browser.html());
-  var projects = browser.queryAll("#checkin");
-  projects.forEach(function(proj) {
-    if (projectValue == proj.value && action == "signin") {
-      browser.choose("signin").pressButton("submit", function() {
-        console.log("Project value :" + proj.value + " signed in successful.");
-        browser.close();
-      });
-    }
+  var targetRadio = browser.query("input[value='" + pid + "']");
+  browser.choose('input[value="' + pid + '"]').pressButton("submit", function() {
+    console.log("Project value :" + pid + " signed in.");
+    browser.close();
   });
 }
 
 function doSignOut(error) {
-  console.log(browser.html());
   browser.choose("signout").pressButton("submit", function() {
+    console.log("Signed out");
     browser.close();
   });
 }
@@ -35,6 +30,7 @@ function doLogin() {
     fill("j_username", id).
     fill("j_password", pw).
     pressButton("Login", function() {
+      console.log("Login");
       if (action == "signin") {
         doSignIn();
       } else if (action == "signout") {
