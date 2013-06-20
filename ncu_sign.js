4a7d7b8987cee7eeb3ceebc1f46eb60f75acc487
Ricky Chien <ricky060709@gmail.com>
2013-06-20 13:59:21 +0800

    Fix CRLF

--- ncu_sign.js
+++ ncu_sign.js
@@ -1,4 +1,4 @@
-﻿var Browser = require("zombie");
+var Browser = require("zombie");
 var assert = require("assert");
 var argv = require('optimist').argv;
 
@@ -9,18 +9,26 @@ var pw = argv._[2];
 var pid = argv._[3];
 var browser = new Browser();
 
-function doSignIn(error) {
-  browser.choose('input[value="' + pid + '"]').pressButton("submit", function() {
+function doSignIn() {
+  browser.
+  choose('input[value="' + pid + '"]', chooseError).
+  pressButton("submit", function() {
     console.log("Project value :" + pid + " signed in.");
     browser.close();
   });
 }
 
-function doSignOut(error) {
-  browser.choose("signout").pressButton("submit", function() {
+function doSignOut() {
+  browser.
+  choose("signout").
+  pressButton("submit", function() {
     console.log("Signed out");
     browser.close();
   });
+}
+
+function chooseError(error) {
+  console.log("test: " + error);
 }
 
 function doLogin() {
