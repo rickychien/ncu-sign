c0234200396117410ef6c09d827e1dd3d25a70ec
Ricky Chien <ricky060709@gmail.com>
2013-06-21 14:50:50 +0800

    Fix visiting website fail problem.

--- ncu_sign.js
+++ ncu_sign.js
@@ -10,9 +10,9 @@ var browser = new Browser();
 
 function doArgvCheck() {
   if (argv.length != 6) {
-    console.log("Usage: node ncu_sign.js Action ID PASSWD ProjectID.\n" + 
-                "Action: signin, signout.\n" +
-                "ProjectID: input button value of NCU signin website.");
+    console.log("Usage: node ncu_sign.js Action ID PASSWD ProjectID.\n" +
+      "Action: signin, signout.\n" +
+      "ProjectID: input button value of NCU signin website.");
     return false;
   }
 
@@ -20,30 +20,46 @@ function doArgvCheck() {
 }
 
 function doSignIn() {
-  browser.
-  choose('input[value="' + pid + '"]').
-  pressButton("submit", function() {
-    console.log("Project value :" + pid + " signed in.");
-    browser.close();
+  browser.visit(signURL + action).
+  then(function() {
+    browser.
+    choose('input[value="' + pid + '"]').
+    pressButton("submit", function() {
+      console.log("Project value :" + pid + " signed in.");
+      browser.close();
+    });
+  }).
+  fail(function() {
+    console.log("Sign in fail, retry to sign in...");
+    doSignIn();
   });
 }
 
 function doSignOut() {
-  browser.
-  choose("signout").
-  pressButton("submit", function() {
-    console.log("Signed out");
-    browser.close();
+  browser.visit(signURL + action).
+  then(function() {
+    browser.
+    choose("signout").
+    pressButton("submit", function() {
+      console.log("Signed out");
+      browser.close();
+    });
+  }).
+  fail(function() {
+    console.log("Sign out fail, retry to signout...");
+    doSignOut();
   });
 }
 
 function doLogin() {
-  browser.visit(signURL + action, function() {
+  browser.visit(signURL + action).
+  then(function() {
     browser.
     fill("j_username", id).
     fill("j_password", pw).
     pressButton("Login", function() {
-      console.log("Login");
+      console.log("Login... done!");
+
       if (action == "signin") {
         doSignIn();
       } else if (action == "signout") {
@@ -52,6 +68,10 @@ function doLogin() {
         console.log("Invalid command");
       }
     });
+  }).
+  fail(function() {
+    console.log("Visiting website fail, retry...");
+    doLogin();
   });
 }
 
