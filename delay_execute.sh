fd90805ee52d34eb584bc9c6bf25d6a648305c1e
Ricky Chien <ricky060709@gmail.com>
2013-06-24 21:18:51 +0800

    Fix basedir path problem when execute nodejs command.

--- delay_execute.sh
+++ delay_execute.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
 MINUTES=$[($RANDOM%25)]
 echo "Now will execute script after $MINUTES minutes."
 sleep $MINUTES
-node ncu_sign.js $1 $2 $3 $4 
+node $BASEDIR/ncu_sign.js $1 $2 $3 $4 
