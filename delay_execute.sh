658421c13417c6c7e91f34792eda99246ba57abb
Fntsrlike <fntsrlike@gmail.com>
2014-11-25 17:34:53 +0800

    Change delay time more anthropopathic

--- delay_execute.sh
+++ delay_execute.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
-SEC=$[($RANDOM % 25) * 60]
-sleep $SEC
-node $BASEDIR/ncu_sign.js $1 $2 $3 $4 
+
+MIN=$[($RANDOM % 10)] # Random Number 0-10 for minutes
+SEC=$[($RANDOM % 60)] # Random Number 0-60 for seconds
+TIME=$[$MIN*60+$SEC]  # Sleep Time
+
+sleep $TIME
+node $BASEDIR/ncu_sign.js $1 $2 $3 $4
